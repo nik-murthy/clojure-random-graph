b4580bd551c4775e6b8b308398a2f405b8e76106
Your Name <nmurthy@icm-consulting.com.au>
2020-02-17 03:39:46 +1100

    Implemented Dijkstras algorithm and ensured that random-graph gives correct output

--- src/ar_test/graph.clj
+++ src/ar_test/graph.clj
@@ -1,17 +1,8 @@
-(ns ar-test.graph)
+(ns ar-test.graph
+  (require [clojure.data.priority-map :refer [priority-map]]))
 
 ;seq for assigning weights (or distances) to edges in graph
 (def weights (vec (range 1 50)))
-
-;Using positive infinity as starting point of shortest path calculation
-(def ^:private inf Double/POSITIVE_INFINITY)
-
-(defn get-random-node
-  "Given a graph, retrieves a random node. Graph must contain at least one node."
-  [graph]
-  (->> (keys graph)
-       (vec)
-       (rand-nth)))
 
 (defn add-node
   "Given a node and a graph, adds the node to it. Works for empty graph.
@@ -34,6 +25,23 @@
     (-> (get graph source-node)
         (assoc target-node distance))))
 
+(defn get-possible-target-nodes
+  "Given a graph and node in the graph, returns all the possible nodes that given
+  node can be connected to"
+  [graph node]
+  (->> (keys graph)
+       (remove (set (list node)))
+       (remove (if (node graph)
+                 (node graph)
+                 (set nil)))))
+
+(defn get-random-node
+  "Given a graph, retrieves a random node. Graph must contain at least one node."
+  [graph]
+  (->> (keys graph)
+       (vec)
+       (rand-nth)))
+
 (defn random-add-new-vertex
   "Given a node, pulls a random existing node from given graph and
   adds an edge with a random weight assigned to the edge"
@@ -51,17 +59,6 @@
       new-graph
       (recur (rest vertices) (random-add-new-vertex
                                new-graph (first vertices))))))
-
-
-(defn get-possible-target-nodes
-  "Given a graph and node in the graph, returns all the possible nodes that given
-  node can be connected to"
-  [graph node]
-  (->> (keys graph)
-       (remove (set (list node)))
-       (remove (if (node graph)
-                 (node graph)
-                 (set nil)))))
 
 (defn already-connected?
   "Given a graph and 2 nodes in it, returns the weight of edge between them,
@@ -103,13 +100,14 @@
 (defn add-all-edges
   "Given a graph where all nodes (or vertices) have been added and connects nodes not
   yet connected until the number of edges required is complete"
-  [graph additional-edges]
-  (loop [count additional-edges
-         finished-graph graph]
-    (if (= 0 count)
+  [graph num-edges]
+  (loop [finished-graph graph]
+    (if (= num-edges (->> finished-graph
+                          (map second)
+                          (map count)
+                          (reduce +)))
       finished-graph
       (recur
-        (- count 1)
         (random-add-new-edge finished-graph (get-random-node graph))))))
 
 (defn random-graph
@@ -120,7 +118,6 @@
                            (map str)
                            (map keyword))
         max-edges (quot (* num-vertices (- num-vertices 1)) 2)
-        additional-edges (- (+ num-edges 1) num-vertices)
         initial-graph (add-node {} (first vertices-coll))
         graph-with-vertices (add-all-vertices initial-graph (rest vertices-coll))]
     (cond
@@ -131,79 +128,112 @@
       (prn "Not a valid graph. Number of edges should be greater than or equal to number of nodes.")
 
       :else
-      (add-all-edges graph-with-vertices additional-edges))))
+      (add-all-edges graph-with-vertices num-edges))))
+
+(defn explore-node
+  "Given a graph, a node to be explored, a map of previously explored nodes, and
+  the current cost at the given node, returns the neighbors with the cumulative cost
+  from the source
+  Test case: get-neighbor-nodes-with-cumulative-weights"
+  [graph visited-nodes current-node current-cost]
+  (let [unexplored-neighbors (->> (current-node graph)
+                                  (remove visited-nodes))]
+    (into {} (for [node-neighbor unexplored-neighbors]
+               [(first node-neighbor) [(+ current-cost
+                                          (get-in graph [current-node (first node-neighbor)]))
+                                       current-node]]))))
+
+(defn get-all-paths
+  "Given a graph and a source node, finds the path to all the other nodes
+  in the graph. The resulting data structure looks as following -
+  ([:1 0 [:1]] [:2 3[:1]])
+  The first value is the destination node, followed by weight/distance and final vector
+  is the path from the source node (the shortest path to :2 is from :1
+  in this example)"
+  [graph source-node]
+  ((fn traverse-graph
+     [visited-nodes next-nodes]
+     (lazy-seq
+       (when-let [[current-node [total-cost previous-node]] (peek next-nodes)]
+         (let [path (conj (vec (visited-nodes previous-node)) current-node)
+               visited-nodes (assoc visited-nodes current-node path)
+               neighbor-nodes (explore-node graph visited-nodes current-node total-cost)
+               next-nodes (merge-with min-key
+                                      (pop next-nodes)
+                                      neighbor-nodes)]
+           (cons [current-node total-cost path]
+                 (traverse-graph visited-nodes next-nodes))))))
+   {}
+   (priority-map source-node [0])))
 
 (defn D
   "Given a graph, a source node and a destination node, returns the list of nodes
   which forms the shortest path between the source and destination nodes"
   [graph source-node destination-node]
-  )
+  (let [not-destination? (fn [[vertex _]]
+                           (not= vertex destination-node))
+        all-paths (get-all-paths graph source-node)]
+    (-> (drop-while not-destination? all-paths)
+        (first)
+        (nth 2))))
 
 (comment
+  (let [graph {:1 {:2 2 :5 9}
+               :2 {:3 1 :4 6 :5 3}
+               :3 {:4 2}
+               :4 {:5 1}}
+        traversed-nodes {:1 [:1]}
+        current-node :2
+        current-cost 2
+        unexplored-nodes (->> (current-node graph)
+                              (remove traversed-nodes))]
+    (explore-node graph traversed-nodes current-node current-cost))
 
-  (let [num-vertices 10
-        vertices-coll (->> (range 1 (+ num-vertices 1))
-                           (map str)
-                           (map keyword))
-        initial-graph (add-node {} (first vertices-coll))
-        graph-with-vertices (add-all-vertices initial-graph (rest vertices-coll))]
-    graph-with-vertices
-    )
+  (peek (priority-map :1 [0]))
 
+  (let [graph {:1 {:2 2 :5 9}
+               :2 {:3 1 :4 6 :5 3}
+               :3 {:4 2}
+               :4 {:5 1}}
+        next-nodes (priority-map :1 [0])
+        visited-nodes {}
+        [current-node [total-cost previous-node]] (peek next-nodes)
+        path (conj (vec (visited-nodes previous-node)) current-node)
+        visited-nodes (assoc visited-nodes current-node path)
+        neighbor-nodes (explore-node graph visited-nodes current-node total-cost)
+        next-nodes (merge-with min-key
+                               (pop next-nodes)
+                               neighbor-nodes)]
+    next-nodes)
 
-  (loop [vertex '(:2 :3 :4)
-         initial-map {:1 nil}]
-    (if (empty? vertex)
-      initial-map
-      (recur (rest vertex) (random-add-new-vertex initial-map (first vertex)))))
+  (let [graph {:1 {:2 10, :3 49, :4 16}
+               :2 {:3 10}
+               :3 {:5 24}
+               :4 nil
+               :5 nil}]
+    (D graph :1 :5))
 
+  (let [graph {:1 {:2 2 :5 9}
+               :2 {:3 1 :4 6 :5 3}
+               :3 {:4 2}
+               :4 {:5 1}}]
+    (get-all-paths graph :1))
 
-  (let [graph {:10 nil,
-               :4  {:6 28},
-               :7  nil,
-               :1  {:2 5, :4 45},
-               :8  nil,
-               :9  nil,
-               :2  {:3 32, :5 14, :9 21},
-               :5  {:7 17, :8 4, :10 6},
-               :3  nil,
-               :6  nil}
-        random-node (get-random-node graph)
-        possible-target-nodes (get-possible-target-nodes graph random-node)
-        ]
-    (remove-already-connected
-      graph
-      possible-target-nodes
-      random-node))
+  (let [graph (random-graph 5 10)]
+    (D graph :1 :4))
 
-  (let [graph {:10 nil,
-               :4  {:6 28},
-               :7  nil,
-               :1  {:2 5, :4 45},
-               :8  nil,
-               :9  nil,
-               :2  {:3 32, :5 14, :9 21},
-               :5  {:7 17, :8 4, :10 6},
-               :3  nil,
-               :6  nil}
-        node :4
-        possible-target-nodes (get-possible-target-nodes graph node)
-        target-node (->> node
-                         (remove-already-connected
-                           graph possible-target-nodes)
-                         (vec)
-                         (rand-nth))
-        new-graph (random-add-new-edge graph node)]
-    new-graph)
+  (random-graph 5 10)
 
-  (let [num-edges 5
-        num-vertices 11]
-    (> num-vertices (quot (* num-edges (- num-edges 1)) 2)))
+  (let [graph {:1 {:2 8, :3 31, :4 12}, :2 nil, :3 {:4 40}, :4 nil}]
+    (->> graph
+         (map second)
+         (map count)
+         (reduce +)))
 
-  (let [n 5
-        e 4]
-    (random-graph n e))
-
+  (let [graph {:1 {:2 8, :3 31, :4 12}, :2 nil, :3 {:4 40}, :4 nil}]
+    (->> graph
+         (keys)
+         (count)))
   )
 
 
--- test/ar_test/graph_test.clj
+++ test/ar_test/graph_test.clj
@@ -2,42 +2,88 @@
   (:require [clojure.test :refer :all]
             [ar-test.graph :refer :all]))
 
-(deftest create-first-node
+(deftest test-create-first-node
   "Create a graph with one node in it"
   (is (=
         (add-node nil :1)
         {:1 nil})))
 
-(deftest add-node-to-existing-graph
+(deftest test-add-node-to-existing-graph
   "Add new node to an existing graph."
   (is (=
         (add-node {:1 nil} :2)
         {:1 nil :2 nil})))
 
-(deftest add-existing-node-to-graph
+(deftest test-add-existing-node-to-graph
   "Add a node to the graph that already exists in the graph.
    Must return unmodified graph."
   (is (=
         (add-node {:1 nil :2 nil} :2)
         {:1 nil :2 nil})))
 
-(deftest connect-two-nodes-in-empty-graph
+(deftest test-connect-two-nodes-in-empty-graph
   "Add the given two nodes to a new graph, connect them and
   capture distance."
   (is (=
         (add-edge nil :1 :2 5)
         {:1 {:2 5} :2 nil})))
 
-(deftest add-new-edge-between-existing-nodes
+(deftest test-add-new-edge-between-existing-nodes
   "Given a graph with existing source node and target node
    that are not connected, add a new edge between them"
   (is (=
         (add-edge {:1 {:2 1} :2 {:3 5} :3 {:4 4} :4 nil} :1 :3 3)
         {:1 {:2 1 :3 3} :2 {:3 5} :3 {:4 4} :4 nil})))
 
-(deftest update-distance-between-nodes
+(deftest test-update-distance-between-nodes
   "Given a graph with 2 nodes connected by a certain distance,
    update the distance between them with the new value given"
   (is (=
         (add-edge {:1 {:2 1} :2 {:3 5} :3 nil} :1 :2 3)
         {:1 {:2 3} :2 {:3 5} :3 nil})))
+
+(deftest test-number-of-vertices-in-graph
+  "Given a number of vertices and edges, checks whether random-graph function
+  outputs a graph with the correct number of vertices"
+  (is (=
+        (->> (random-graph 5 10)
+             (keys)
+             (count))
+        5)))
+
+(deftest test-number-of-edges-in-graph
+  "Given a number of vertices and edges, checks whether random-graph function
+  outputs a graph with the correct number of edges"
+  (is (=
+        (->> (random-graph 5 10)
+             (map second)
+             (map count)
+             (reduce +))
+        10)))
+
+(deftest test-neighbor-nodes-with-cumulative-weights
+  "Given a traversed set of nodes at a vertex, and the neighbors of the vertex,
+  checks if the map returned contains the neighboring nodes and cumulative distance
+  from source"
+  (let [graph {:1 {:2 2 :5 9}
+              :2 {:3 1 :4 6 :5 3}
+              :3 {:4 2}
+              :4 {:5 1}}
+        traversed-nodes {:1 [:1]}
+        current-node :2
+        current-cost 2]
+    (is (=
+          (explore-node graph traversed-nodes current-node current-cost)
+          {:3 [3 :2], :4 [8 :2], :5 [5 :2]}))
+    ))
+
+(deftest test-shortest-path
+  "Given a graph and 2 nodes, test if function D returns the shortest path
+  between the two"
+  (let [graph {:1 {:2 2 :5 9}
+                     :2 {:3 1 :4 6 :5 3}
+                     :3 {:4 2}
+                     :4 {:5 1}}]
+    (is (=
+          (D graph :1 :5)
+          [:1 :2 :5]))))
